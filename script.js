dff924b809e63563bbf80aff8571e07cd1904546
kristinemayg-lgtm <kristinemayg@gmail.com>
2025-10-01 11:26:43 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,78 @@
+let number = 5;
+
+if (number > 0) {
+    console.log("The number is positive.");
+} else if (number < 0) {
+    console.log("The number is negative.");
+} else {
+    console.log("The number is zero.");
+}
+
+let day = 4
+
+switch (day) {
+    case 1:
+        console.log("Sunday");
+        break;
+    case 2:
+        console.log("Monday");
+        break;
+    case 3:
+        console.log("Tuesday");
+        break;
+    case 4:
+        console.log("Wednesday");
+        break;
+    case 5:
+        console.log("Thursday");
+        break;
+    case 6:
+        console.log("Friday");
+        break;
+    case 7:
+        console.log("Saturday");
+        break;
+    default:
+            console.log("Invalid");
+}
+
+for (let i = 1; i <= 5; i++) {
+    console.log("print i");
+}
+let i = 1;
+
+while (i <= 5) {
+    console.log("Print i and increment");
+    break;
+}
+
+let j = 1;
+
+do {
+    console.log("Print j j++");
+    break;
+} while (j <= 5);
+
+    if (i === 3) {
+    console.log("Use break here");
+
+}
+console.log(i)
+
+if (i === 3) {
+    console.log("Use continue here");
+}
+
+let globalVar = "I'm global!";
+
+function scopeExample() {
+    let localVar = "I'm local!";
+    console.log("Inside function:");
+    console.log(globalVar);
+    console.log(localVar);
+}
+
+scopeExample();
+
+console.log("Outside function:");
+console.log(globalVar);
